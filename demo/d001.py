10d8c88252ff1bd8d5751f40a0491fd3e8bf2e47
Thomas Wessel <mrvelle@gmail.com>
2025-06-19 11:39:19 +0200

    001

---
+++ demo/d001.py
@@ -0,0 +1,15 @@
+import dbf
+
+table = dbf.Table(
+        filename='000.dbf',
+        field_specs='name C(25); age N(3,0); birth D; qualified L',
+        )
+
+table.open(dbf.READ_WRITE)
+
+for datum in (
+        ('Spanky', 7, dbf.Date.fromymd('20010315'), False),
+        ('Spunky', 23, dbf.Date(1989, 7, 23), True),
+        ('Sparky', 99, dbf.Date(), dbf.Unknown),
+        ):
+    table.append(datum)
